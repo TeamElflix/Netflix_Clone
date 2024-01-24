235a8f9d57810abbf710eec902105d72912fb8ef
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-01-25 01:31:49 +0900

    feat: assistance.html 파일 생성 및 기본 내용  추가

--- main-login/login.js
+++ main-login/login.js
@@ -1,10 +1,1 @@
-// const input_email = document.getElementById("email-or-phoneNumber");
-// const input_password = document.getElementById("password");
 
-// input_email.addEventListener("focus", function () {
-//   input_email.style.visibility = "visible"; // 선택되었을 때 placeholder 텍스트 표시
-// });
-
-// input_password.addEventListener("focus", function () {
-//   input_password.style.visibility = "visible"; // 선택되었을 때 placeholder 텍스트 표시
-// });
