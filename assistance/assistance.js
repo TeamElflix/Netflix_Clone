1ca0a86503cefcd863602d04b1c15ce65dbee5d6
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-02-03 00:49:46 +0900

    feat: radio button 클릭시 내용 구성 바뀌게

--- assistance/assistance.js
+++ assistance/assistance.js
@@ -13,3 +13,33 @@ window.onload = function () {
     select.appendChild(el);
   }
 };
+
+document.addEventListener("DOMContentLoaded", function () {
+  // ID로 라디오 버튼 가져오기
+  var emailRadio = document.getElementById("select");
+  var smsRadio = document.getElementById("select2");
+
+  // 클래스로 div 엘리먼트 가져오기
+  var emailDiv = document.querySelector(".email-select");
+  var smsDiv = document.querySelector(".sms-select");
+
+  // "이메일 주소" 라디오 버튼을 초기에 선택하도록 설정
+  emailRadio.checked = true;
+
+  // 초기에 "이메일 주소" 라디오 버튼에 대한 처리
+  emailDiv.style.display = "block";
+  smsDiv.style.display = "none";
+
+  // 라디오 버튼에 이벤트 리스너 추가
+  emailRadio.addEventListener("click", function () {
+    // "이메일 주소" 라디오 버튼이 클릭되면 email-select div를 표시하고 sms-select div를 숨김
+    emailDiv.style.display = "block";
+    smsDiv.style.display = "none";
+  });
+
+  smsRadio.addEventListener("click", function () {
+    // "문자 메시지(SMS)" 라디오 버튼이 클릭되면 sms-select div를 표시하고 email-select div를 숨김
+    emailDiv.style.display = "none";
+    smsDiv.style.display = "block";
+  });
+});
