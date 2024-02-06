5d4f013d835449cbdb1c6ad886bf611d5d96bcc1
wwwaas0 <127373635+wwwaas0@users.noreply.github.com>
2024-02-06 10:50:30 +0900

    feat: login 이메일, 비밀번호 검증 추

--- main-login/login.js
+++ main-login/login.js
@@ -35,3 +35,58 @@ document.addEventListener("DOMContentLoaded", function () {
     detailButton.style.display = "none";
   });
 });
+
+/*이메일, 비밀번호 조건검사*/
+window.onload = function () {
+  // 이메일 조건검사
+  var emailInput = document.getElementById("email");
+  var emailErrorDiv = document.getElementById("emailError");
+
+  function handleEmailBlur() {
+    var emailValue = emailInput.value;
+
+    // 간단한 이메일 형식을 검사하는 정규표현식
+    var emailRegex = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+
+    console.log("Email value:", emailValue);
+    if (emailValue.trim() === "") {
+      emailInput.classList.add("invalid");
+      emailErrorDiv.textContent = "이메일 주소는 반드시 입력하셔야 합니다.";
+      emailErrorDiv.classList.add("inputError");
+    } else if (!emailRegex.test(emailValue)) {
+      emailInput.classList.add("invalid");
+      emailErrorDiv.textContent = "올바른 이메일 주소를 입력하세요.";
+      emailErrorDiv.classList.add("inputError");
+    } else {
+      emailErrorDiv.textContent = "";
+      emailErrorDiv.classList.remove("inputError");
+    }
+  }
+
+  emailInput.addEventListener("blur", handleEmailBlur);
+
+  // 비밀번호 조건검사
+  var passwordInput = document.getElementById("password");
+  var passwordErrorDiv = document.getElementById("passwordError");
+
+  passwordInput.addEventListener("blur", handlePasswordBlur);
+
+  function handlePasswordBlur() {
+    var passwordValue = passwordInput.value;
+
+    if (passwordValue.trim() === "") {
+      // 빈 칸인 경우
+      passwordInput.classList.add("invalid");
+      passwordErrorDiv.textContent = "비밀번호는 반드시 입력하셔야 합니다.";
+    } else if (passwordValue.length < 6 || passwordValue.length > 60) {
+      // 글자 수가 6자 미만 또는 60자 초과인 경우
+      passwordInput.classList.add("invalid");
+      passwordErrorDiv.textContent = "비밀번호는 6~60자 사이여야 합니다.";
+    } else {
+      // 유효한 경우 에러 메시지 숨김
+      if (passwordErrorDiv) {
+        passwordErrorDiv.textContent = "";
+      }
+    }
+  }
+};
